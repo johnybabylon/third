dafb00bf9e7076ef9aed6c7568bc6beda2149aa8
johny babylon <ztredded@gmail.com>
2015-06-22 14:29:55 -0700

    Initial Commit

---
+++ third/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  third
+//
+//  Created by elliott chavis on 6/22/15.
+//  Copyright (c) 2015 elliott chavis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ third/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  third
+//
+//  Created by elliott chavis on 6/22/15.
+//  Copyright (c) 2015 elliott chavis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
